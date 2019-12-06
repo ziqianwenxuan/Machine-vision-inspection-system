a7082c67c418ad66d785f41eb81d2bca5449b739
ziqianwenxuan <316240451@qq.com>
2019-12-06 17:22:26 +0800

    第一次更新，建立框架

---
+++ InspectionSystem/mainwindow.cpp
@@ -0,0 +1,53 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "qdebug.h"
+
+#pragma execution_character_set("utf-8")
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    //日期/时间显示
+    QTimer *timer = new QTimer(this);
+    connect(timer,SIGNAL(timeout()),this,SLOT(timerUpdate()));
+    timer->start(1000); //1000ms 触发一次
+    openpicture();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+void MainWindow::timerUpdate()
+{
+    QDateTime time = QDateTime::currentDateTime();
+
+    QString str = time.toString("yyyy-MM-dd hh:mm:ss dddd");
+
+    ui->dateTime->setText(str);
+
+}
+
+void MainWindow::openpicture()
+{
+    qDebug()<<QString("显示图片");
+    QGraphicsScene *scene = new QGraphicsScene(this);
+    ui->date_view->setScene(scene);
+    scene->addPixmap( QPixmap("1.png") );
+
+    ui->date_view->show();
+    qDebug()<<QString("图片没有显示");
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+//    ui->label_2->setText(tr("Critical Message Box"));
+//    qDebug()<<QString("12345");
+    ui->textBrowser->setText(tr("Information Message Box"));
+    QMessageBox::information(this,tr("Information"),tr("RUNING Test"));
+    return;
+}
+
+
---
+++ InspectionSystem/mainwindow.h
@@ -0,0 +1,29 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QMessageBox>
+#include <QTimer>
+#include <QDateTime>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();
+    void timerUpdate();
+private:
+    Ui::MainWindow *ui;
+    void  openpicture();
+
+};
+#endif // MAINWINDOW_H
